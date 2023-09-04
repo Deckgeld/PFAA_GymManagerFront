63a1e54d81f45a8130d0787db9b2b7dc5b54e3c3
Cristian B <cristianjbc99@gmail.com>
2023-09-04 00:02:47 -0600

    Invoicing page

--- PF_GymManagerFront/src/app/pages/invoicing/invoicing.component.ts
+++ PF_GymManagerFront/src/app/pages/invoicing/invoicing.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-invoicing',
@@ -7,4 +9,37 @@ import { Component } from '@angular/core';
 })
 export class InvoicingComponent {
 
+  @ViewChild('pdfContent', { static: false }) pdfContent!: ElementRef;
+
+  generatePDF() {
+    const pdfContent = this.pdfContent.nativeElement;
+
+    html2canvas(pdfContent).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+
+      
+      const imgWidth = 210; 
+      const pageHeight = 297;
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save('invoice.pdf');
+    });
+  }
+
+
+  
 }
