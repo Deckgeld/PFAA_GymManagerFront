066db4959cefdc9a776c4ce0ca517456e8ecf536
Tu nombre <utm210008ti@ut-morelia.edu.mx>
2024-04-24 20:04:29 -0600

    Update .gitignore and environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,7 +6,7 @@ export const environment = {
 
     hasSession:false,
 
-    baseUrl: 'https://localhost:7025/',
+    baseUrl: 'https://pfagymmanagerbackend-production.up.railway.app/',
     httpOptions : {
         headers:new HttpHeaders({
           'Content-Type': 'application/json'
